569c0b0b3f645ce980bf645f080ec24e608f7712
Iipal <malkoleyplay@gmail.com>
2019-08-17 18:34:01 +0300

    simple ray marching engine

--- includes/rmv1.h
+++ includes/rmv1.h
@@ -2,5 +2,54 @@
 # define RMV1_H
 
 # include <stdio.h>
+# include <sys/types.h>
+
+# include "libvectors.h"
+# include "libftsdl.h"
+
+# define NUM_OF_STEPS   5000
+# define MIN_HIT_DIST   0.001
+# define MAX_TRACE_DIST 1000.0
+
+# define WIN_X 1000
+# define WIN_Y 500
+
+# define CANVAS_STARTX -(WIN_X / 2)
+# define CANVAS_STARTY -(WIN_Y / 2)
+# define CANVAS_ENDX   WIN_X / 2
+# define CANVAS_ENDY   WIN_Y / 2
+
+# define FROM_CANVAS(point, w_size) ((point) * (w_size) / (1000.0 * w_size))
+
+# define STEP_SIZE 0.1f
+
+extern __v4df	ro; /* represents Ray Origin */
+extern __v4df	rd; /* represents Ray Direction */
+
+extern __v4df	s; /* represents Sphere position */
+extern float_t	sr; /* represents Sphere radius */
+
+typedef struct	s_time
+{
+	float_t	old;
+	float_t	current;
+	float_t	res;
+}	Time;
+
+typedef struct	s_env
+{
+	Sdl		*sdl;
+	Time	fps;
+}	Environment;
+
+extern uint32_t	g_flags;
+
+# define OUT_FPS_COUNTER (1U << 0U)
+
+# define SET_BIT(mask, bit) ((mask) |= (bit))
+# define UNSET_BIT(mask, bit) ((mask) &= ~(bit))
+# define TOGGLE_BIT(mask, bit) ((mask) ^= (bit))
+
+# define IS_BIT(mask, bit) ((mask) & (bit))
 
 #endif
--- srcs/main.c
+++ srcs/main.c
@@ -1,6 +1,110 @@
 #include "rmv1.h"
 
+__v4df	ro = { 0.0, 0.0, -5.0, 0.0 };
+__v4df	rd = { 0.0, 0.0, 1.0, 1.0 };
+
+__v4df	s = { 0.0, 0.0, 0.0, 0.0 };
+float_t	sr = 1.0f;
+
+uint32_t	g_flags = 0U;
+
+/*
+ * p: arbitrary point in 3D space
+ * c: sphere center
+ * r: sphere radius
+ *
+ * SDF(Signed Distance Function), return value means point 'p' is:
+ *  negative: inside sphere.
+ *  positive: outside sphere.
+ *  zero:     on the sphere surface.
+ */
+float_t	dst_from_sphere(__v4df const p, __v4df const c, float_t const r)
+{
+	return v_len(p - c) - r;
+}
+
+Color	ray_march(void)
+{
+	float_t	total_dist	= 0.0f;
+
+	for (size_t i = 0; NUM_OF_STEPS > i; ++i) {
+		__v4df	curr_pos = ro + total_dist * rd;
+
+		float_t	dist_to_closest = dst_from_sphere(curr_pos, s, sr);
+
+		if (MIN_HIT_DIST > dist_to_closest)
+			return (Color){ .c = { 255, 128, 0 } };
+
+		if (MAX_TRACE_DIST < total_dist)
+			break ;
+
+		total_dist += dist_to_closest;
+	}
+	return (Color){ 0x0 };
+}
+
+static void	render_ray_march(Sdl *restrict const sdl)
+{
+	__v2df	i;
+	for (Y(i) = CANVAS_STARTY; CANVAS_ENDY > Y(i); Y(i)++) {
+		for (X(i) = CANVAS_STARTX; CANVAS_ENDX > X(i); X(i)++) {
+			__v2df	uv = { Y(i) / 1000.0, X(i) / 1000.0 };
+			rd = (__v4df){X(uv), Y(uv), 1.0, 0.0};
+			Color	pxl = ray_march();
+			if (pxl.hex)
+				sdl_pixelput(sdl->wsurf,
+					CONVERT_FROM_CANVAS(X(i), Y(i), WIN_X, WIN_Y), pxl);
+		}
+	}
+}
+
+static inline void	key_press(SDL_Keycode const key)
+{
+	if (SDLK_z == key)
+		TOGGLE_BIT(g_flags, OUT_FPS_COUNTER);
+}
+
+static inline void	fps_counter(Time *const fps)
+{
+	fps->old = fps->current;
+	fps->current = SDL_GetTicks();
+	fps->res = (fps->current - fps->old) / 1000.0f;
+}
+
+static void	render_loop(Environment *restrict const env)
+{
+	bool		quit = false;
+	Sdl *const	sdl = env->sdl;
+
+	while (!quit)
+	{
+		while (0 < SDL_PollEvent(&sdl->e))
+			if (SDL_QUIT == sdl->e.type)
+				quit = true;
+			else if (SDL_KEYDOWN == sdl->e.type)
+			{
+				if (SDLK_ESCAPE == sdl->e.key.keysym.sym)
+					quit = true;
+				else
+					key_press(sdl->e.key.keysym.sym);
+			}
+		SDL_FillRect(sdl->wsurf, NULL, 0xfaf4f2);
+		render_ray_march(sdl);
+		SDL_UpdateWindowSurface(sdl->w);
+		if (IS_BIT(g_flags, OUT_FPS_COUNTER))
+			printf("ms: %.4f\n", env->fps.res);
+		fps_counter(&env->fps);
+	}
+}
+
 int	main(void)
 {
-	printf("Hello, World!\n");
+	Environment	*env = calloc(1, sizeof(*env));
+	env->sdl = calloc(1, sizeof(*env->sdl));
+	if (!sdl_init(env->sdl, WIN_X, WIN_Y, "Ray Marching"))
+		return -1;
+	render_loop(env);
+	sdl_free(&env->sdl);
+	free(env->sdl);
+	free(env);
 }
